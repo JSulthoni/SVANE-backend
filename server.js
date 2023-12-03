71132d716f168b3c56c551d4d5b7f796c55e7051
JSulthoni <sulthonijavier@gmail.com>
2023-12-03 14:05:59 +0700

    added homepage

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 import express from 'express';
-const app = express();
 import mongoose from 'mongoose';
-mongoose.set('strictQuery', false);
 import dotenv from 'dotenv';
-dotenv.config();
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import productRoute from './router/productRoute.js';
+import stripeRoute from './router/stripeRoute.js';
+import subcategoryRoute from './router/subcategoryRoute.js';
+mongoose.set('strictQuery', false);
+dotenv.config();
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
-// Import Route
-import productRoute from './router/productRoute.js';
-import stripeRoute from './router/stripeRoute.js';
-import subcategoryRoute from './router/subcategoryRoute.js';
+// Setting file directories from url to path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
-// Middleware
+
+// Routes
 app.use('/api/products', productRoute);
 app.use('/api/stripe', stripeRoute);
 app.use('/api/subcategory', subcategoryRoute);
@@ -22,7 +30,7 @@ app.use('/api/subcategory', subcategoryRoute);
 // Error Handling
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500
-    const errorMessage = err.message || 'We are sorry, something went wrong'
+    const errorMessage = err.message || 'This is the error handler, something went wrong'
     return res.status(errorStatus).json({
         success: false,
         status: errorStatus,
@@ -33,8 +41,8 @@ app.use((err, req, res, next) => {
 
 
 // Server homepage
-app.get('/' , (req, res) => {
-    res.send(`Welcome, this is SVANE backend server. Running on port ${PORT}`)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'server.html'))
 });
 
 
@@ -43,7 +51,7 @@ const PORT = process.env.PORT || 3000; // PORT cyclic 3000
 const URI = process.env.VITE_MONGO_API_URL;
 mongoose.connect(URI)
     .then(() => {
-        console.log(`Connecting to ${URI}`)
+        console.log(`Connecting to database`)
         app.listen(PORT, "0.0.0.0" , () => {
             console.log(`Connection successful on port ${PORT}`)
         })
